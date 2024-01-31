ea8c5767ad1cf81b9dd8df42878e3a73bf1ea977
Amantry7 <omurzakovomon@gmail.com>
2024-01-31 20:07:37 +0600

    hw1

---
+++ hw1/hw1.js
@@ -0,0 +1,16 @@
+const number = 14;
+console.log(number);
+
+let name = 'Omon';
+name = 'Aman';
+console.log(name)
+
+const age = 16;
+console.log(typeof age);
+
+// 5, 5.1, Nan, -1, +1, Infinity == number(int)
+// True, False == boolaen
+// '', "", `` == string(str)
+// очень большие числа == BigInt
+// разные симфалы  == Symbol
+// нечего или пустой == undefined, null
