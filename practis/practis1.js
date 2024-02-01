c8827e4295a3fef609440e37b07b5d22bfa38528
Amantry7 <omurzakovomon@gmail.com>
2024-02-01 12:29:41 +0600

    up

---
+++ practis/practis1.js
@@ -0,0 +1,28 @@
+// const name = 'Aman';
+// console.log(name);
+
+let name = 'Абдулох';
+name = 'Пупсик';
+console.log(name);
+
+const im_lox = false;
+console.log(im_lox)
+
+const number = 2312;
+console.log(number);
+
+const num = 1111n;
+console.log(typeof num);    
+
+
+const list = ['Молоко', 'Яйца','Хлеб', 'Мясо']
+console.log(list);
+list.splice(3);
+console.log(list);
+
+console.log(list[2]);
+list[4] = 'Рыба';
+console.log(list);
+
+const ffff = null;
+console.log(ffff);
